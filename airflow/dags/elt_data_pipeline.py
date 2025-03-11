e214e52baf117584d78087f0980ec251e4dd54b3
dain55788 <dain55788@gmail.com>
2025-03-11 10:01:27 +0700

    Update files and directories

--- airflow/dags/elt_data_pipeline.py
+++ airflow/dags/elt_data_pipeline.py
@@ -1,4 +1,12 @@
 # AIRFLOW DAGS for processing ELT pipeline from Bronze to Gold Layer
-# Task dependencies: start_pipeline --> extract_load --> transform_data --> delta_convert --> end_pipeline
+# Task dependencies: start_pipeline --> transform_data --> delta_convert --> end_pipeline
+
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator #Dummy is replaced with EmptyOperator
+
 
 
---
+++ scripts/transform_data.py
@@ -0,0 +1,7 @@
+# Performing data transformation in from MinIO Bronze --> Silver
+
+import os
+import sys
+import pandas as pd
+from minio import Minio
+import time
